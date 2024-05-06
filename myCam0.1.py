3c380117b8ebef0d54837d9405e7feb9fd205f6f
jasperverplanke <verplanke.jasper@gmail.com>
2024-05-06 09:25:25 +0200

    newest
    idk ik hoop dat dit de goede fov is

--- myCam0.1.py
+++ myCam0.1.py
@@ -16,13 +16,15 @@ toZumo = Pin("PA9", Pin.OUT_PP)
 
 sensor.reset()  # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.RGB565)  # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.QVGA)  # Set frame size to QVGA (320x240)
+sensor.set_framesize(sensor.HVGA)  # Set frame size to QVGA (320x240)
 sensor.skip_frames(time=2000)  # Wait for settings take effect.
 clock = time.clock()  # Create a clock object to track the FPS.
-sensor.set_vflip(enable)
+sensor.set_vflip(True)
+#sensor.set_lens_correction(True, 200, 20)
+sensor.ioctl(sensor.IOCTL_SET_FOV_WIDE, True)
 
 while True:
     clock.tick()  # Update the FPS clock.
     img = sensor.snapshot()  # Take a picture and return the image.
     print(clock.fps())  # Note: OpenMV Cam runs about half as fast when connected
-    # to the IDE. The FPS should increase once disconnected.
+
