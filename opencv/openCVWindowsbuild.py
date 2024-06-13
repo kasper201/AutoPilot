64d04d5f5591b5d01e69eeb49e97b7345f310d5f
jasperverplanke <verplanke.jasper@gmail.com>
2024-06-13 16:09:21 +0200

    good kruispunt herkening

--- opencv/openCVWindowsbuild.py
+++ opencv/openCVWindowsbuild.py
@@ -8,7 +8,7 @@ import signal
 #from edge_impulse_linux.image import ImageImpulseRunner
 
 # Nicla server address
-server_address = "http://192.168.93.29:8080/"
+server_address = "http://192.168.137.173:8080/"
 count = 0
 
 # Create a session object to reuse connections
@@ -21,15 +21,15 @@ if (sys.platform == 'linux' and not os.environ.get('DISPLAY')):
     show_camera = False
 
 # Define roi pixel amounts
-twoLinesFront = 103
-oneLineBack = 106
+twoLinesFront = 60
+oneLineBack = 40
 twoLinesBack = 10
 
 # Define the regions of interest (ROIs)
-roiFront = (60, 60, 340, 5)
-roiBack = (100, 112, 240, 5)
-rightRoiBack = (240, 112, 120, 5)
-leftRoiBack = (100, 112, 180, 5)
+roiFront = (60, 20, 380, 5)
+roiBack = (100, 95, 240, 5)
+rightRoiBack = (240, 95, 120, 5)
+leftRoiBack = (100, 95, 180, 5)
 roiLeft = (100, 60, 150, 50)
 roiRight = (240, 60, 150, 50)
 
@@ -82,16 +82,14 @@ def getDirection(img):
     calc = round((us_average / 5) * -1)
 
     if calc > 15:
-	    calc = 15
+        calc = 15
     elif calc < -23:
-	    calc = -23
+        calc = -23
 
     #if calc > -4 and calc < 4:
     #    calc = 0
     
     return  calc + 100
-
-
 
 
 
@@ -113,6 +111,8 @@ def send_command(command):
             imageDetection(image)
             #cv2.imwrite('image', cv2.cvtColor(image , cv2.COLOR_RGB2BGR))
             # Display the image in a window
+            
+            cv2.rectangle(image ,roiLineBack,3 )
             cv2.imshow('Image', image)
             cv2.waitKey(1)
             #return image_data
@@ -189,7 +189,23 @@ def imageDetection(image):
 
         #center(gray_img)
 	speed = 120
-	turn = getDirection(binary_img)
+	road_type = detect_road_type(img)
+	if (road_type ==  "Unknown"):
+		turn = getDirection(binary_img)
+	elif (road_type == "Straight line"):
+		turn = getDirection(binary_img)
+	elif (road_type == "Left turn"):
+		turn = getDirection(binary_img)
+	elif (road_type == "Right turn"):
+		turn = getDirection(binary_img)
+	elif (road_type == "t_Intersection"):
+		turn = getDirection(binary_img)
+	elif (road_type == "Intersection"):
+		turn = getDirection(binary_img)
+	else:
+		speed = 0
+		print("WHWUH ERROR NOT A KNOWN ROAD TYPE")
+        
 	control = speed * 200 + turn
 	command = "Integer=" + str(control)
 	send_command(command)
@@ -197,6 +213,46 @@ def imageDetection(image):
 	# the image will be resized and cropped, save a copy of the picture here
 	# so you can see what's being passed into the classifier
 	#cv2.imwrite('debug.jpg', cv2.cvtColor(cropped_w, cv2.COLOR_RGB2BGR))
+
+
+def detect_road_type(img):
+    #img = cv2.imread(image_path)
+    
+    # Crop the top 160 rows
+    cropped_img = img[:160, :]
+    
+    # Convert to grayscale
+    gray_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
+    
+    # Apply binary threshold to get a binary image (black road becomes white)
+    _, binary_img = cv2.threshold(gray_img, 101, 255, cv2.THRESH_BINARY_INV)
+    
+    # Draw ROIs on the image for visualization
+    color_img = cv2.cvtColor(binary_img, cv2.COLOR_GRAY2BGR)
+    cv2.rectangle(color_img, (roiFront[0], roiFront[1]), (roiFront[0] + roiFront[2], roiFront[1] + roiFront[3]), (0, 255, 0), 2)
+    cv2.rectangle(color_img, (roiBack[0], roiBack[1]), (roiBack[0] + roiBack[2], roiBack[1] + roiBack[3]), (0, 0, 255), 2)
+    cv2.rectangle(color_img, (rightRoiBack[0], rightRoiBack[1]), (rightRoiBack[0] + rightRoiBack[2], rightRoiBack[1] + rightRoiBack[3]), (255, 0, 0), 2)
+    cv2.rectangle(color_img, (leftRoiBack[0], leftRoiBack[1]), (leftRoiBack[0] + leftRoiBack[2], leftRoiBack[1] + leftRoiBack[3]), (255, 255, 0), 2)
+    cv2.rectangle(color_img, (roiBack2[0], roiBack2[1]), (roiBack2[0] + roiBack2[2], roiBack2[1] + roiBack2[3]), (255, 255, 255), 2)
+    
+    # Determine road type based on ROIs
+    road_type = "Unknown"
+    if straightLine(binary_img):
+        road_type = "Straight line"
+    elif leftTurn(binary_img):
+        road_type = "Left turn"
+    elif rightTurn(binary_img):
+        road_type = "Right turn"
+    elif t_intersection(binary_img):
+        road_type = "t_Intersection"
+    elif intersection(binary_img):
+        road_type = "Intersection"
+       # Display the road type on the image
+    cv2.putText(color_img, road_type, (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.imshow("Road detection", color_img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    return road_type
 
 
 def main(argv):
