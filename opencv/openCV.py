7edd42531b590e36136a2262c4b94a027f264a66
Jasper Verplanke <146102893+jasperverplanke@users.noreply.github.com>
2024-06-20 13:01:26 +0200

    Update openCV.py ai no-re-init

--- opencv/openCV.py
+++ opencv/openCV.py
@@ -8,8 +8,9 @@ import signal
 from edge_impulse_linux.image import ImageImpulseRunner
 
 # Nicla server address
-server_address = "http://192.168.172.29:8080/"
+server_address = "http://192.168.155.29:8080/"
 count = 0
+
 
 # Create a session object to reuse connections
 session = requests.Session()
@@ -165,8 +166,8 @@ def help():
 
 def signNumbers(labels):
     return {'50':"SIGN Integer=4",
-            'B01':"SIGN Integer=5",
-            'b02':"",
+            'B01':"",
+            'b02':"SIGN Integer=5",
             'b07':"SIGN Integer=6",
             'c02':"",
             'c04':"SIGN Integer=7",
@@ -182,58 +183,59 @@ def signNumbers(labels):
 def imageDetection(image):
     modelfile = os.path.join(dir_path, model)
     img = image
+    global runner
     if img is None:
         print('Failed to load image', args[1])
         exit(1)
-
-    with ImageImpulseRunner(modelfile) as runner:
-        try:
+    if (runner is None):
+        with ImageImpulseRunner(modelfile) as runner:
+            #try:
             model_info = runner.init()
             print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
             labels = model_info['model_parameters']['labels']
 
-            img = image
-            if img is None:
-                print('Failed to load image', args[1])
-                exit(1)
+    img = image
+    if img is None:
+        print('Failed to load image', args[1])
+        exit(1)
 
-            # imread returns images in BGR format, so we need to convert to RGB
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            
-            #Prevent image from being cropped by adding a border below it
-            img = cv2.copyMakeBorder(img, 0, 160, 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+    # imread returns images in BGR format, so we need to convert to RGB
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            # get_features_from_image also takes a crop direction arguments in case you don't have square images
-            features, cropped = runner.get_features_from_image(img)
+    #Prevent image from being cropped by adding a border below it
+    img = cv2.copyMakeBorder(img, 0, 160, 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
 
-            res = runner.classify(features)
-            if len(res["result"]["bounding_boxes"]) >= 1:
-                print('Sign found!')
+    # get_features_from_image also takes a crop direction arguments in case you don't have square images
+    features, cropped = runner.get_features_from_image(img)
 
-            if "classification" in res["result"].keys():
-                print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
-                for label in labels:
-                    score = res['result']['classification'][label]
-                    print('%s: %.2f\t' % (label, score), end='')
-                print('', flush=True)
+    res = runner.classify(features)
+    if len(res["result"]["bounding_boxes"]) >= 1:
+        print('Sign found!')
 
-            elif "bounding_boxes" in res["result"].keys():
-                print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
-                for bb in res["result"]["bounding_boxes"]:
-                    # Send string of label
-                    send_command(signNumbers(bb['label']))
-                    print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
-                    cropped = cv2.rectangle(cropped, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
+    if "classification" in res["result"].keys():
+        print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
+        for label in labels:
+            score = res['result']['classification'][label]
+            print('%s: %.2f\t' % (label, score), end='')
+        print('', flush=True)
 
-            if "visual_anomaly_grid" in res["result"].keys():
-                print('Found %d visual anomalies (%d ms.)' % (len(res["result"]["visual_anomaly_grid"]), res['timing']['dsp'] + res['timing']['classification']))
-                for grid_cell in res["result"]["visual_anomaly_grid"]:
-                    print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (grid_cell['label'], grid_cell['value'], grid_cell['x'], grid_cell['y'], grid_cell['width'], grid_cell['height']))
-                    cropped = cv2.rectangle(cropped, (grid_cell['x'], grid_cell['y']), (grid_cell['x'] + grid_cell['width'], grid_cell['y'] + grid_cell['height']), (255, 125, 0), 1)
-            # Line detection
-        finally:
-            if (runner):
-                runner.stop()
+    elif "bounding_boxes" in res["result"].keys():
+        print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
+        for bb in res["result"]["bounding_boxes"]:
+            # Send string of label
+            send_command(signNumbers(bb['label']))
+            print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
+            cropped = cv2.rectangle(cropped, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
+
+    if "visual_anomaly_grid" in res["result"].keys():
+        print('Found %d visual anomalies (%d ms.)' % (len(res["result"]["visual_anomaly_grid"]), res['timing']['dsp'] + res['timing']['classification']))
+        for grid_cell in res["result"]["visual_anomaly_grid"]:
+            print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (grid_cell['label'], grid_cell['value'], grid_cell['x'], grid_cell['y'], grid_cell['width'], grid_cell['height']))
+            cropped = cv2.rectangle(cropped, (grid_cell['x'], grid_cell['y']), (grid_cell['x'] + grid_cell['width'], grid_cell['y'] + grid_cell['height']), (255, 125, 0), 1)
+        # Line detection
+    #finally:
+        #   if (runner):
+            #   runner.stop()
 
     # imread returns images in BGR format, so we need to convert to RGB
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
